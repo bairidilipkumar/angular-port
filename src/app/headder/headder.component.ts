0b879c0f6200d0801c167a4baeead6a5471d2cee
bairidilipkumar <bairidilipkumar@gmail.com>
2024-03-23 17:37:48 -0500

    angular-port first push

---
+++ src/app/headder/headder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headder',
+  standalone: true,
+  imports: [],
+  templateUrl: './headder.component.html',
+  styleUrl: './headder.component.css'
+})
+export class HeadderComponent {
+
+}
