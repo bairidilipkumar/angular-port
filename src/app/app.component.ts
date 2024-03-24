3ab26ca54bd655c05970bd50c7c15007d4367262
bairidilipkumar <bairidilipkumar@gmail.com>
2024-03-24 13:31:22 -0500

    added main page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeadderComponent } from './headder/headder.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeadderComponent,FooterComponent],
+  imports: [RouterOutlet,HeadderComponent,FooterComponent,MainpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
